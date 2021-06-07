5a2f787ea93e6a2fbfaf8c48b15ff3fdc5234b95
lutchenca <lutchenca@hotmail.com>
2021-06-07 13:32:38 -0300

    first commit

---
+++ exercicios.js
@@ -0,0 +1,15 @@
+//modulo 1 - introducao ao node 
+
+// 1 - importação de módulos
+const moduloNativo = require('http');
+const moduloInstalado = require('axios');
+const moduloProprio = require('./minhaFuncao');
+
+//2 cachorro Scooby
+let cachorro = {
+    nome: 'Scooby',
+    idade: 7,
+    gostaDeComer: true
+}
+
+module.exports = cachorro;
