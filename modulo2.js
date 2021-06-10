b2d785b44b9d2c4e81bfba136a24eefb252c7e9f
lutchenca <lutchenca@hotmail.com>
2021-06-10 14:36:07 -0300

    atualizacao exercicios

--- modulo2.js
+++ modulo2.js
@@ -13,3 +13,35 @@ let cachorro = {
 }
 
 module.exports = cachorro;
+
+
+//Variaveis
+var exemplo = function () {
+    return 'Oi, eu sou uma função expressa';
+}
+
+//IDADE_MINIMA e MAXIMA
+const IDADE_MINIMA = 18;
+const IDADE_MAXIMA = 99;
+
+//declaracao de variaveis
+let verdadeiro = true;
+let nada = null;
+let texto = "texto";
+let numero = 10;
+
+//condicionais
+let dado = true;
+if (dado === true) {
+    console.log('É verdadeiro!')
+} else {
+    console.log('É falso!')
+}
+
+// condicionais - javascript
+let linguagem = "javascript"
+if (linguagem === "javascript") {
+    console.log('Estou aprendendo')
+} else {
+    console.log('Vou aprender mais pra frente')
+}
