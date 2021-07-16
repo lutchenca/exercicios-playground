9c3ddd4a80c845a70ddc5ac8891193b2496cb937
Lutchenca Medeiros <lutchenca@hotmail.com>
2021-07-16 10:02:31 -0300

    modulo 3 update

--- modulo3.js
+++ modulo3.js
@@ -1,1 +1,16 @@
-// modulo 3 - metodologias de desenvolvimento
+// modulo 3 - git
+
+//require http
+const http = require('http');
+
+//criando um servidor 
+http.createServer(function (req, res) {
+    console.log('iniciando servidor');
+  });
+
+//listen
+const http = require('http');
+
+http.createServer((req, res) =>  {
+	console.log("Instanciando um servidor");
+}).listen(3000, 'localhost');
