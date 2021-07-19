db96483545b53a390ae173d14ac5d9d7c5f38b0d
Lutchenca Medeiros <lutchenca@hotmail.com>
2021-07-19 15:12:55 -0300

    express exercises

--- modulo3.js
+++ modulo3.js
@@ -14,3 +14,19 @@ const http = require('http');
 http.createServer((req, res) =>  {
 	console.log("Instanciando um servidor");
 }).listen(3000, 'localhost');
+
+//express
+const express = require('express');
+const app = express();
+
+//levantando o servidor
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Levantando um servidor com Express');
+});
+
+app.listen(8000, () =>
+console.log('Levantando um servidor com Express')
+);
